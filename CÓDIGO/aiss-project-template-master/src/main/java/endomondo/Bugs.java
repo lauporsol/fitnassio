144e7797391b1d5c3713021b16105255af431e60
natal <natal@LAPTOP-F2EBHCLJ>
2019-04-04 20:18:40 +0200

    Dependencies y bugs sin fallos

--- CÓDIGO/aiss-project-template-master/src/main/java/endomondo/Bugs.java
+++ CÓDIGO/aiss-project-template-master/src/main/java/endomondo/Bugs.java
@@ -8,9 +8,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "url" })
@@ -41,28 +39,6 @@ public class Bugs {
 		this.additionalProperties.put(name, value);
 	}
 
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this).append("url", url).append("additionalProperties", additionalProperties)
-				.toString();
-	}
 
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(additionalProperties).append(url).toHashCode();
-	}
-
-	@Override
-	public boolean equals(Object other) {
-		if (other == this) {
-			return true;
-		}
-		if ((other instanceof Bugs) == false) {
-			return false;
-		}
-		Bugs rhs = ((Bugs) other);
-		return new EqualsBuilder().append(additionalProperties, rhs.additionalProperties).append(url, rhs.url)
-				.isEquals();
-	}
 
 }
